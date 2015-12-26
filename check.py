4adb2d6b81a8f370f7f0b1e2dad7716bc43edbeb
Ted Roby <git@sranet.com>
2015-12-25 23:06:43 -0800

    add remove_duplicates()

---
+++ check.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3.4
+import requests, re, bs4
+
+# request index
+# get all anchors
+# find hrefs
+
+# clean url list
+# remove duplicates
+def remove_duplicates(list):
+  new_list=[]
+  for url in list:
+    if url not in new_list:
+      new_list.append(url)
+  return new_list
+# remove non-http URIs
