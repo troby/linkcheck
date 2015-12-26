190d1c3f7f9fb5c1fc3538b2e20427f32fe301e1
Ted Roby <git@sranet.com>
2015-12-25 23:15:50 -0800

    add test for remove_nonhttp_uri()

--- test.py
+++ test.py
@@ -21,3 +21,15 @@ try:
   print('remove_duplicates() OK')
 except:
   print('remove_duplicates() FAIL')
+
+try:
+  new = check.remove_nonhttp_uri(list)
+  assert len(new) == 5
+  assert 'http://site.com/link.html' in list
+  assert 'http://site.com/site.html' in list
+  assert 'https://site.com/index.html' in list
+  assert 'http://site.com/link.html' in list
+  assert 'static.html' in list
+  print('remove_nonhttp_uri() OK')
+except:
+  print('remove_nonhttp_uri() FAIL')
