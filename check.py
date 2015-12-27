c5473fa78fc8297be67299a3de54ae00b5b97691
Ted Roby <git@sranet.com>
2015-12-26 16:23:22 -0800

    update documentation

--- check.py
+++ check.py
@@ -1,11 +1,10 @@
 #!/usr/local/bin/python3.4
 import requests, re, bs4
 
-# globals
 class SiteChecker:
   '''
     Tool for checking links of a site.
-    Invoke with check = SiteChecker('http://mysite.com')
+    Invoke with check = SiteChecker('mysite.com')
     self.sitename:     string containing base url of site to check
     self.visited:      list of urls already visited
     self.missing:      list of urls resulting in 404 response
@@ -13,6 +12,11 @@ class SiteChecker:
   '''
 
   def __init__(self, url):
+    '''
+      Provide url for testing.
+      If a lazy name (like google.com) is provided,
+      prepend http:// and append backslash.
+    '''
     if not re.match(r'^http(s)?://.*', url):
       url = 'http://' + url
     if not re.match(r'.*/$', url):
@@ -23,8 +27,13 @@ class SiteChecker:
   missing = []
   pruned  = []
 
-  # clean url list
   def prune_uris(self, list):
+    '''
+      take a list of anchor href links and cleanup as follows:
+        * remove duplicates
+        * convert relative paths to absolute URIs
+        * forget about non-HTTP URI paths - like javascript:void()
+    '''
     for url in list:
       if url not in self.pruned:
         self.pruned.append(url)
@@ -40,11 +49,11 @@ class SiteChecker:
         if ('http://' in url) or ('https://' in url):
           self.pruned.append(url)
 
+### TODO ###
 # request page
 # get all anchors
 # find hrefs
-# clean urls
-# save urls
 # crawl urls for more urls
 # remove previously visited urls
-# track 404s
+# verify external links exist, but do not crawl those pages
+# remove reoccurances of sitename
