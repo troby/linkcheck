33bd637023e66ada16e08c6bde4af8aad9bcb7fb
Ted Roby <git@sranet.com>
2015-12-27 00:46:41 -0800

    fix visited test and add test for sitename with html path

--- test.py
+++ test.py
@@ -16,7 +16,8 @@ list = [
 try:
   mysite = linkcheck.SiteChecker('mysite.com')
   assert mysite.sitename == 'http://mysite.com/'
-  assert mysite.visited == ['http://mysite.com/']
+  assert mysite.base_url == 'http://mysite.com/'
+  assert mysite.visited == ['http://mysite.com/','http://mysite.com','http://mysite.com/']
   assert mysite.missing == []
   assert mysite.pruned == []
   print('class initialization OK')
@@ -46,14 +47,23 @@ try:
   assert len(mysite.pruned) == 5
   assert 'http://mysite.com/visited.html' not in mysite.pruned
   print('remove visited OK')
+  list.remove('http://mysite.com/visited.html')
 except:
   print('remove visited FAIL')
 
 # test sitename with html path
 try:
+  del mysite
   mysite = linkcheck.SiteChecker('mysite.com/index.html')
   assert mysite.sitename == 'http://mysite.com/index.html'
-  assert mysite.domain == 'mysite.com'
+  assert mysite.base_url == 'http://mysite.com/'
+  mysite.prune_uris(list)
+  assert len(mysite.pruned) == 5
+  assert 'http://site.com/link.html' in mysite.pruned
+  assert 'http://site.com/site.html' in mysite.pruned
+  assert 'https://site.com/index.html' in mysite.pruned
+  assert 'http://mysite.com/static.html' in mysite.pruned
+  assert 'http://mysite.com/about.html' in mysite.pruned
   print('set self.domain OK')
 except:
   print('set self.domain FAIL')
