e25ecea515edcdddc94b3b94a3bea03127b8c9b5
Ted Roby <git@sranet.com>
2015-12-28 16:02:54 -0800

    configure functional test for basic localhost site with missing links and sublinks

--- functional_test.py
+++ functional_test.py
@@ -4,7 +4,8 @@ import linkcheck
 lc=linkcheck.SiteChecker('127.0.0.1')
 lc.verbose=True
 lc.start()
-assert lc.last_status == 200
-assert len(lc.pruned) == 2
-assert 'http://127.0.0.1/download.html' in lc.pruned
-assert 'http://127.0.0.1/missing_1.html' in lc.pruned
+assert len(lc.visited) == 6
+assert len(lc.missing) == 2
+assert len(lc.pruned) == 0
+assert 'http://127.0.0.1/missing_1.html' in lc.missing
+assert 'http://127.0.0.1/missing_2.html' in lc.missing
